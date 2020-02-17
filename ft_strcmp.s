2c1c9df8217866a9403a71e82b3790848bb1aefa
Romain Challier <rchallie@student.42.fr>
2020-02-17 18:49:48 +0100

    Update ft_strcmp.s

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -13,14 +13,14 @@ compa:
 	je		exit		; jump to exit
 	mov		al, BYTE [rdi]	; get the least significant byte in rdi where is stock the value of the char
 	mov		bl, BYTE [rsi]	; get the least significant byte in rsi where is stock the value of the char
-	cmp al, bl				; compare al and bl
-	jne exit				; if the result of cmp is diferrent than 0 so the string are differents
-	inc rdi					; increment the rdi pointer
-	inc rsi					; increment the rsi pointer
-	jmp compa				; if we are here it's because the chars of string are equals the we can continu
+	cmp 		al, bl		; compare al and bl
+	jne 		exit		; if the result of cmp is diferrent than 0 so the string are differents
+	inc 		rdi		; increment the rdi pointer
+	inc 		rsi		; increment the rsi pointer
+	jmp 		compa		; if we are here it's because the chars of string are equals the we can continu
 	
 exit:
-	movzx rax, al			; movzx = copy a register of inferior size in a bigger and fill the other bits with 0, and this register is rax
-    movzx rbx, bl			; same that previous but set it in rbx
-    sub rax, rbx			; stock the difference of rax and rbx in rax; Finaly : do the difference beetween the char at the rdi pointer and the char at the rsi pointer 
-	ret						; return (rax)
+	movzx 	rax, al			; movzx = copy a register of inferior size in a bigger and fill the other bits with 0, and this register is rax
+    	movzx 	rbx, bl			; same that previous but set it in rbx
+    	sub 	rax, rbx		; stock the difference of rax and rbx in rax; Finaly : do the difference beetween the char at the rdi pointer and the char at the rsi pointer 
+	ret				; return (rax)
